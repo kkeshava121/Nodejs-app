1d2416abb721ceade0ace4740b85db0a4ab444d9
root <root@instance-20250128-095754.us-central1-c.c.spherical-treat-449209-n0.internal>
2025-01-29 07:06:40 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// Import the HTTP module
+const http = require('http');
+
+// Create a server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200; // HTTP status code (200 = OK)
+  res.setHeader('Content-Type', 'text/plain'); // Set response header
+  res.end('Hello, World!\n'); // Send response
+});
+
+// Define the port to listen on
+const port = 3000;
+
+// Start the server
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, Node.js is running on Ubuntu 22.04!');
+});
+
+const PORT = 3000;
+const HOST = '0.0.0.0'; // Change from localhost to 0.0.0.0
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server is running on http://${HOST}:${PORT}`);
+});
+
